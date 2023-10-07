af27ffb8a8487bff6103c7e48537cc917505c4a6
Marty Penner <martypenner@users.noreply.github.com>
2023-10-07 23:12:00 +0000

    add openai

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,18 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    import OpenAI from 'openai';
+
+	const openai = new OpenAI();
+
+	const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: 'You are Ben. You produce very mundane and simple facts based on prios knowledge. For example, you might respond with: "When cutting a turkey, a sharp knife helps you cut better!" You always respond with at most 2 sentences. Your style resembles facts from the Red Green show. You never ask question, but are always cheerful. Only answer with a fact; don't include a preamble.' }],
+      model: 'gpt-4',
+    });
+
+    console.log(chatCompletion.choices);
+
+	let fact = chatCompletion.choices[0].message.content;
 </script>
 
 <svelte:head>
@@ -19,6 +31,10 @@
 		</span>
 
 		to Ben's facts!
+
+		<p>New fact:</p>
+
+		<p>{fact}</p>
 	</h1>
 </section>
 
