c0d7ea9b7615b542c0b06e663a4cda632042f558
Marty Penner <martypenner@users.noreply.github.com>
2024-02-04 21:02:04 -0500

    feat: the snail is now a button. that fetches new facts.

--- packages/website/app/routes/_index.tsx
+++ packages/website/app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import { getRandomFact } from '@bens-facts/core';
 import { json, LoaderFunctionArgs } from '@remix-run/cloudflare';
-import { useLoaderData } from '@remix-run/react';
+import { useFetcher, useLoaderData } from '@remix-run/react';
 import bensFacts from '~/images/bens-facts-logo.png';
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
@@ -13,7 +13,9 @@ export const loader = async ({ context }: LoaderFunctionArgs) => {
 };
 
 export default function Index() {
-	const { fact } = useLoaderData<typeof loader>();
+	const { fact: initialFact } = useLoaderData<typeof loader>();
+	const factFetcher = useFetcher<{ fact: string }>();
+	const fact = factFetcher?.data != null ? factFetcher.data.fact : initialFact;
 
 	return (
 		<section>
@@ -28,9 +30,26 @@ export default function Index() {
 									</h1>
 									<div className="flex flex-col items-start space-y-4">
 										<p className="max-w-[700px] text-gray-500 md:text-xl">{fact}</p>
-										<div className="space-x-4">
-											<SnailIcon className="h-12 w-12" />
-										</div>
+										<factFetcher.Form method="get">
+											<button type="submit" className="space-x-4 hover:text-blue-400">
+												<svg
+													className="h-12 w-20 move squish"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 24 24"
+													fill="none"
+													stroke="currentColor"
+													strokeWidth="2"
+													strokeLinecap="round"
+													strokeLinejoin="round"
+												>
+													<path d="M2 13a6 6 0 1 0 12 0 4 4 0 1 0-8 0 2 2 0 0 0 4 0" />
+													<circle cx="10" cy="13" r="8" />
+													<path d="M2 21h12c4.4 0 8-3.6 8-8V7a2 2 0 1 0-4 0v6" />
+													<line className="wiggle" x1="18" y1="3" x2="19.1" y2="5.2" />
+													<line className="wiggle" x1="22" y1="3" x2="20.9" y2="5.2" />
+												</svg>
+											</button>
+										</factFetcher.Form>
 									</div>
 								</div>
 								<img alt="Hero" src={bensFacts} width="300" />
@@ -41,34 +60,10 @@ export default function Index() {
 
 				<footer className="h-20 flex items-center px-4 md:px-6 border-t">
 					<p className="text-xs text-gray-500 dark:text-gray-400">
-						© {new Date().getFullYear()} Ben's Facts Inc. All rights reserved.
+						© {new Date().getFullYear()} Ben&apos;s Facts Inc. All rights reserved.
 					</p>
 				</footer>
 			</div>
 		</section>
-	);
-}
-
-function SnailIcon(props) {
-	return (
-		<svg
-			{...props}
-			className={`${props.className} snail`}
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-		>
-			<path d="M2 13a6 6 0 1 0 12 0 4 4 0 1 0-8 0 2 2 0 0 0 4 0" />
-			<circle cx="10" cy="13" r="8" />
-			<path d="M2 21h12c4.4 0 8-3.6 8-8V7a2 2 0 1 0-4 0v6" />
-			<path className="snail__antenna" d="M18 3 19.1 5.2" />
-			<path className="snail__antenna" d="M22 3 20.9 5.2" />
-		</svg>
 	);
 }
