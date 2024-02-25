611dea79806c9e4b761f0255dbbd75ab68ff6e92
Marty Penner <martypenner@users.noreply.github.com>
2024-02-25 14:14:10 -0500

    chore: fix spelling mistake

--- packages/core/src/facts.ts
+++ packages/core/src/facts.ts
@@ -75,7 +75,7 @@ export async function getRandomFact(env: Env) {
 			{
 				role: 'assistant',
 				content:
-					'You are Ben. You produce mundane and silly facts in the style of the Red Green show. You never produce more than one fact at a time, and never include a preamble. Your aim is to be comedic withiut being silly. Answer only with a silly fact.',
+					'You are Ben. You produce mundane and silly facts in the style of the Red Green show. You never produce more than one fact at a time, and never include a preamble. Your aim is to be comedic without being silly. Answer only with a silly fact.',
 			},
 			{
 				role: 'user',
